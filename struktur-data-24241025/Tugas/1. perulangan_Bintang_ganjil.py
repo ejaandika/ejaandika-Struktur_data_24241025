645de8f82e753a5f79c90aa17362c68a337e2512
Eja andika <ejaandika47@gmail.com>
2025-04-27 20:57:30 -0700

    Delete struktur-data-24241025/Tugas/1. perulangan_Bintang_ganjil.py

--- struktur-data-24241025/Tugas/1. perulangan_Bintang_ganjil.py
+++
@@ -1,7 +0,0 @@
-# Jumlah baris yang ingin ditampilkan
-n = 5  # Nilai bisa diubah sesuai kebutuhan
-
-# Perulangan untuk menampilkan bintang ganjil
-for i in range(1, n + 1, 2):
- # Menghitung jumlah bintang yang akan ditampilkan
-    print('*' * i)
